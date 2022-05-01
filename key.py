5b03984520679ac0fcfe6913471aa4fc3d36d6ea
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-05-01 12:24:12 -0400

    Update key.py

--- key.py
+++ key.py
@@ -1,6 +1,7 @@
 from ply import lex, yacc
 import platform
 import subprocess
+import subprocess, sys
 import json
 import re
 from flask import Flask, render_template
@@ -13,24 +14,20 @@ from requests import get
 import time
 from getmac import get_mac_address
 import requests
-#---------package end------------------------#
 def kill():
   exit(1)
 def hold(x):
   time.sleep(x)
 def bark(i):
   print(i)
-#-------end of base packages-----------------#
 def mac():
   get_mac_address()
 def xip():
   public_ip =  requests.get("http://wtfismyip.com/text").text
   print(public_ip)
-#------end of network------------------------#
 def dmessage(webhook, message):
   discord = Discord(url=webhook)
   discord.post(content=message)
-#----------end of message package------------#
 def webdown(website):
   url = website
   html_output_name = 'download.html'
@@ -38,17 +35,27 @@ def webdown(website):
   with open(html_output_name, 'w') as f:
     f.write(req.text)
     f.close()
-#------end of download page----------------#
 def wificon():
   import con
 def NCHIhelp():
   bark("NCHI is made with python, C++, C, and HTML/JS you are currently in a python shell so you can still run python scripts in a .NCHI file")
-  #-----------end of help------------------#
 def pcinfo():
+  subprocess.Popen("info.exe")
+def tasklist():
   bark("WAITING ON OS")
+#-----------------------------------------#
+#-----------------------------------------#
+#-----------  START OF TOKEN  ------------#
+#-----------------------------------------#
+#-----------------------------------------#
+
+
+
+
+
+
 #-----------------------------------------#
 #-----------------------------------------#
 #-----------  END OF TOKEN  --------------#
 #-----------------------------------------#
 #-----------------------------------------#
-
