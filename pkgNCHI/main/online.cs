9a9d6433c1e29ae7460ae785eb354a0eacacc90e
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-05-18 14:25:36 -0400

    Create online.cs

---
+++ pkgNCHI/main/online.cs
@@ -0,0 +1,28 @@
+using System.Net.NetworkInformation;  
+using System;
+
+private static bool IsMachineUp(string hostName)
+{
+    bool retVal = false;
+    try
+    {
+        Ping pingSender = new Ping();
+        PingOptions options = new PingOptions();
+        options.DontFragment = true;
+        string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        byte[] buffer = Encoding.ASCII.GetBytes(data);
+        int timeout = 120;
+ 
+        PingReply reply = pingSender.Send(hostName, timeout, buffer, options);
+        if (reply.Status == IPStatus.Success)
+        {
+            retVal = true;
+        }
+    }
+    catch (Exception ex)
+    {
+        retVal = false;
+        Console.WriteLine(ex.Message);
+    }
+    return retVal;
+}
