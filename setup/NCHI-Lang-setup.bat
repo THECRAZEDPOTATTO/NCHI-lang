a8042e558a5ba99697faf06b2d198cbe99c14b51
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-05-18 14:46:24 -0400

    Create NCHI-Lang-setup.bat

---
+++ setup/NCHI-Lang-setup.bat
@@ -0,0 +1,8 @@
+echo "MAIN SET UP FOR WINDOWS";
+clear 
+echo "All files with a .nchi .nch .nachi will be truned into  a NCHI-Lang file and will run like a python shell"
+echo""
+echo"For someone else to run a .nchi file they will have to have the lang installed along with the needed packages"
+echo""
+echo"NCHI files are ment for client side easy building only all .nchi can be converted to a python file, an exe, and or a c++/c file"
+echo"thank you"
