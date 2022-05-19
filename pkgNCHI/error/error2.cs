321a2c6065de580d4198c972398d4c1b8af4b844
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-05-19 09:30:32 -0400

    Update error2.cs

--- pkgNCHI/error/error2.cs
+++ pkgNCHI/error/error2.cs
@@ -1,1 +1,1 @@
-MessageBox.Show("NCHI file counld not compile", "Comp Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+MessageBox.Show("NCHI file counld not compile some files", "Comp Error", MessageBoxButton.OK, MessageBoxImage.Warning);
